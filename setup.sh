3f9565b87b81cf54562dd7946e8acfc2e7679344
ButzXploit <arifpanji80@gmail.com>
2026-01-17 10:21:48 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+clear
+echo "(!)sabar lagi setup"
+echo ""
+echo ""
+sleep 1
+apt update -y && apt upgrade -y
+pkg install python -y
+pkg install python3 -y
+pkg install curl wget cloudflared -y
+python mainfile.py
