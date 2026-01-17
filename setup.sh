8b9134ba2ff91b0b132382f1c4d6abfe6629d94b
ButzXploit <arifpanji80@gmail.com>
2026-01-17 10:27:45 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,5 +6,5 @@ sleep 1
 apt update -y && apt upgrade -y
 pkg install python -y
 pkg install python3 -y
-pkg install curl wget cloudflared -y
+pkg install curl wget cloudflared jq -y
 python mainfile.py
